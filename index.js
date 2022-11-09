e4e71a0012a8865a77b11acbf54e47daf9e7f858
Chris Bonet <christopher.bonet@gmail.com>
2022-11-09 17:38:44 -0500

    commit tree ab833de47896e95b3f1e34a9a85a667d303562c2

--- index.js
+++ index.js
@@ -1,1 +1,42 @@
-// Write your solution here!
+const cats = ["Milo", "Otis", "Garfield"];
+
+function destructivelyAppendCat(cat) {
+    cats.push('Ralph')
+};
+
+function destructivelyPrependCat(cat) {
+    cats.unshift("Bob")
+};
+
+function destructivelyRemoveLastCat(cat) {
+    cats.pop()
+};
+
+function destructivelyRemoveFirstCat(cat) {
+    cats.shift()
+};
+
+function appendCat(cat) {
+// return [...cats, "Broom"] //
+    let newCats = cats.slice()
+    newCats.push("Broom")
+    return newCats
+};
+
+ function prependCat(cat) {
+    let newCats = cats.slice()
+    newCats.unshift("Arnold")
+    return newCats
+ };
+
+ function removeLastCat(cat) {
+    let newCats = cats.slice()
+    newCats.pop()
+    return newCats
+ };
+
+ function removeFirstCat(cat) {
+    let newCats = cats.slice()
+    newCats.shift()
+    return newCats
+ };
